5d706bd1e983f349b2c7bc7f32346339473539b6
LinaParren <54841398+LinaParren@users.noreply.github.com>
2021-12-14 11:03:23 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,10 @@
-// JavaScript Document
+var deButton = document.querySelector("button");
+
+function doeCategorieNeer(){
+	let deRijen = document.querySelector("article");
+    deRijen.style.display = "block";
+	deRijen.classList.toggle("toonRijen");
+    
+}
+
+deButton.addEventListener("click", doeCategorieNeer);
